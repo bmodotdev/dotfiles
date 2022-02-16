4e682bc066eb415772f87f6829bd1859daa0dc9d
William E Little Jr <git@bmo.dev>
2022-02-16 09:36:01 -0600

    Tidy bashrc files

--- bash/perl.bash
+++ bash/perl.bash
@@ -7,21 +7,25 @@ source ~/perl5/perlbrew/etc/bashrc
 # Aliases #
 ###########
 set_alias p 'perl -Mstrict -MData::Dumper'
+set_alias pb perlbrew
+set_alias pc perlcritic
+set_alias pd perldoc
+set_alias pt perltidy
 
 #############
 # Functions #
 #############
 
 function install_perlbrew () {
-	local bin="${GIT_DIR}App-perlbrew/perlbrew"
-	if [ ! -x "$bin" ]; then
-		>&2 printf 'perlbrew missing or not executable: “%s”\n' "$bin"
-		return
-	fi
+    local bin="${GIT_DIR}App-perlbrew/perlbrew"
+    if [ ! -x "$bin" ]; then
+        >&2 printf 'perlbrew missing or not executable: “%s”\n' "$bin"
+        return
+    fi
 
-	printf 'Perofmring self install...\n'
-	"$bin" self-install
+    printf 'Perofmring self install...\n'
+    "$bin" self-install
 
-	printf 'Performing install-patchperl...\n'
-	"$bin" -f -q install-patchperl
+    printf 'Performing install-patchperl...\n'
+    "$bin" -f -q install-patchperl
 }
