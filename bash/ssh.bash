c404cd9f36ecf6249cce9b5b17722971af354857
William E Little Jr <git@bmo.dev>
2022-03-20 17:58:32 -0500

    Add ssh-agent support

---
+++ bash/ssh.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SSH_AGENT_KEY_LIFE=86400
+export SSH_AGENT_ENV="${HOME}/.ssh/agent.env"
+[ -s "$SSH_AGENT_ENV" ] && source "$SSH_AGENT_ENV"
+
+# `man ssh-add`: Exit status is 0 on success,
+#   1 if the specified command fails,
+#   and 2 if ssh-add is unable to contact the authentication agent.
+ssh-add -l >/dev/null 2>&1
+if [ $? -eq 2 ]; then
+    ssh-agent -s -t "$SSH_AGENT_KEY_LIFE" > "$SSH_AGENT_ENV"
+    source "$SSH_AGENT_ENV"
+fi
